3b8299414384b667190ae1a386d645aba38c4aa8
MaximePrdhm <maxime.prdh@gmail.com>
2022-09-05 14:51:22 +0200

    Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
-unction App() {
+import io from 'socket.io-client'
+
+import './App.css';
+
+const socket = io.connect('http://localhost:3001');
+
+function App() {
   return (
     <div className="App">
     </div>
